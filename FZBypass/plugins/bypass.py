c0efa8e2a99264ab9410427bad4d55c4f73e1e08
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-09-09 18:14:21 +0530

    Update bypass.py

--- FZBypass/plugins/bypass.py
+++ FZBypass/plugins/bypass.py
@@ -66,8 +66,8 @@ async def bypass_check(client, message):
         elif isinstance(result, list):
             bp_link, ui = "", "┖"
             for ind, lplink in reversed(list(enumerate(result, start=1))):
-                bp_link = f"\n{ui} <b>{ind}x Bypass Link:</b> {lplink}" + bp_link
-                bp_link = bp_link
+                #bp_link = f"\n{ui} <b>{ind}x Bypass Link:</b> {lplink}" + bp_link
+                bp_link = f"\n" + bp_link
                 ui = "┠"
         else:
             bp_link = f"\n┖ <b>Bypass Link:</b> {result}"
